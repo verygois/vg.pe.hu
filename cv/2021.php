13af108994fc100e629f23c0d31f8e1a0eabd722
Very Gois <we.are.pe.hu@gmail.com>
2021-08-14 21:01:56 +0900

    vg

---
+++ cv/2021.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>About | Very Gois</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="/cv/logo.png">
+<link rel="stylesheet" href="/css/bio.css"/>
+<script type="text/javascript">
+$(function(){
+$("#").load("");
+})
+</script>
+<style>
+</style>
+</head>
+<body>
+<header>
+<u><b>Very Gois</b></u> aims to disseminate vg things from all over the world.
+</header>
+<div id="bio">
+  <p>すごいと思った物事を集めたオンランコンテンツ／コンピレーション作品集の制作と、音楽会・展覧会などのイベント企画を軸に、自分がすごいと思った物事を紹介することがベリーゴイスの目標です。</p>
+  <h4>2016.4.24 <a href="http://vg.pe.hu/compilation/01/">Very Gois #1</a> とウェブサイト <a href="http://vg.pe.hu">vg.pe.hu</a> を発表<br/>
+    2016.7.10 <a href="http://vg.pe.hu/compilation/02/">Very Gois #2</a> を発表<br/>
+    2017.3.22 <a href="http://vg.pe.hu/compilation/03/">Very Gois #3</a> を発表<br/>
+    2017.10.10 <a href="http://vg.pe.hu/compilation/04/">Very Gois #4</a> を発表</h4>
+  <p>2016年春以降、音楽会・展覧会などのイベント企画を開始　<a href="http://vg.pe.hu/show/">vg.pe.hu/show</a></p>
+  <h4>2017年12月、vg.pe.hu のドメイン名を転用し、クリエイティブ・コミュニティスペース「ペフ」を設立。<br/>
+    2017年12月-2018年4月／2019年1月-8月の二期に渡って、ギャラリー／コミュニティスペースを運営しました。　<a href="http://vg.pe.hu/jp/nisitemma/">vg.pe.hu/jp/nisitemma/</a></h4>
+<h3><em>　</em></h3>
+  <h1>VG +</h1>
+  <p>2016年の秋ごろから、友達にもらったもの・教えてもらったこと、趣味で作ったウェブサイトなどをオンラインコンテンツ <a href="http://vg.pe.hu/up/">VG +</a> として発表開始</p>
+  <h4>2021年4月、ウェブサイトの作り方をまとめたウェブサイト <a href="http://creative-community.pe.hu/coding/">creative-community.pe.hu/coding/</a> を開設しました。</h4>
+<h3><em>　</em></h3>
+<h1>VG CTLG</h1>
+<p>2014年2月に <a href="http://vg.pe.hu/jp/cm/kaochomex/">中尾香織</a> とともにスタートした <a href="https://web.archive.org/web/20160816120030/http://nnnnnnnnnnnnn.web.fc2.com/nn.html">ナイスショップスー</a> を終了し、2021年6月から 世界中のすごい商品を世界中のすごいインディペンデントスペースに流通するディストリビューションサービス <a href="http://vg.pe.hu/ctlg/">VG CTLG</a> を開始</p>
+<h4>2021年6月、ペフが運営するウェブショップ兼ニュースメディア <a href="http://newlifecollection.com/?ca=35">NewLifeCollection</a> 内に、取扱商品や影響を受けたものごとを紹介するコンテンツを開設し、 <a href="http://creative-community.pe.hu/coding/">卸販売</a> を開始ました。</h4>
+</div>
+<footer>
+<h2>direct by <b>Kazuma Sasajima</b></h2>
+</footer>
+</body>
+</html>
