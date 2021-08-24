f5eb9175a60073f9b652307e929e64c1bc59bd63
Very Gois <we.are.pe.hu@gmail.com>
2021-08-24 22:13:34 +0900

    ctlg

---
+++ ctlg/sample/random.js
@@ -0,0 +1,20 @@
+const random = [
+    "___",
+    "___"
+  ];
+  
+  function randomImg(randomArray) {
+    var random =
+    randomArray[Math.floor(Math.random() * randomArray.length)];
+    console.log(random);
+    return random;
+  }
+  function sentenceGenerator() {
+    var sentence = `<img src="${randomImg(random)}">`;
+    document.querySelector(".random").innerHTML = sentence;
+  }
+  window.setInterval(function() {
+    sentenceGenerator();
+  }, 1000);
+  sentenceGenerator();
+  
