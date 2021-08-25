c22ead548d744ddb42f260cbd69634dfe0a102db
Very Gois <we.are.pe.hu@gmail.com>
2021-08-25 20:43:15 +0900

    ctlg

---
+++ ctlg/book/woopheadclrms/random.js
@@ -0,0 +1,20 @@
+const random = [
+  "/ctlg/book/___/001.jpg",
+  "/ctlg/book/___/002.jpg"
+  ];
+  
+  function randomImg(randomArray) {
+    var random =
+    randomArray[Math.floor(Math.random() * randomArray.length)];
+    console.log(random);
+    return random;
+  }
+  function sentenceGenerator() {
+    var sentence = `<img src="${randomImg(random)}">`;
+    document.querySelector(".random").innerHTML = sentence;
+  }
+  window.setInterval(function() {
+    sentenceGenerator();
+  }, 2000);
+  sentenceGenerator();
+  
