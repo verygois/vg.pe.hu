7880533fb3e71961e6b47293e02452648802cc27
Very Gois <we.are.pe.hu@gmail.com>
2021-08-25 17:21:49 +0900

    Update random.js

--- ctlg/book/emeraldclitoris/random.js
+++ ctlg/book/emeraldclitoris/random.js
@@ -1,10 +1,14 @@
 const random = [
-  "/ctlg/book/emeraldclitoris/10005.jpg",
-  "/ctlg/book/emeraldclitoris/dinami.gif",
+  "/ctlg/book/emeraldclitoris/10005.jpg"
   "/ctlg/book/emeraldclitoris/sny4.jpg",
   "/ctlg/book/emeraldclitoris/sny6.jpg",
   "/ctlg/book/emeraldclitoris/sny9.jpg",
-  "/ctlg/book/emeraldclitoris/sny12.jpg"
+  "/ctlg/book/emeraldclitoris/sny12.jpg",
+  "/1f/04/01.jpg",
+  "/1f/04/02.jpg",
+  "/1f/04/03.jpg",
+  "/1f/04/04.jpg",
+  "/1f/04/05.jpg"
   ];
   
   function randomImg(randomArray) {
