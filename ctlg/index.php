2bd72cfe0ce9189546648e13749a374e5af1a0a9
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-11-05 22:29:40 +0900

    Update index.php

--- ctlg/index.php
+++ ctlg/index.php
@@ -3,7 +3,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>VG CTLG</title>
+<title>VG CTLG | Very Gois</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="http://creative-community.pe.hu/coding/js/org.js"></script>
 <script type="text/javascript">
